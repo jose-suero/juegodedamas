98635e5a8bb97f31f4c100556839ca53b4f514f4
Jose Suero <jose.suero@gmail.com>
2016-12-07 00:27:08 -0400

    Cambios realizados por José Suero. 1. Se rediseño el javascript para más eficiencia. 2. Se agregaron los objetos <img> a un <div> debajo del <div> principal    del tablero para garantizar que el movimiento de cada <img> se vea    por encima de los cuadros del tablero. 3. Se configuró la opción size para identificar el tamaño del tablero. 4. Se agregó la función delegada para el evento drop y se definió    código para que la <img> se establezca al centro del cuadro en    que se dejó caer.

--- js/widgetdamas.js
+++ js/widgetdamas.js
@@ -1,90 +1,84 @@
 $.widget("custom.juegodedamas", {
 		
 	options: {
-		player1: {name: "Jugador 1", color: "#FF0000"},
-		player1: {name: "Jugador 2", color: "#0000FF"}
+		"player1": { name: "Jugador 1" },
+		"player1": { name: "Jugador 2" },
+		"size": 960		
 	},
 	
 	//Constructor
 	_create: function () {
+		//Para lograr referenciar a este widget dentro de funciones que redefinen la palaba clave this
+		var thisWidget = this;
+		var cellsize = this.options.size / 8;
 		
-		//inicializar tablero.
-		this.element.addClass("tablero");
+		$(["tablaMain", "tablaObjs"]).each(function(i, e) {
+			thisWidget._appendToElement($("<DIV>").prop({
+				"id": e
+			}));
+		});
 		
-		var lastRow;
-		var lastCol;
+		this._getMainDiv().css({
+			"overflow": "auto",
+			"width": this.options.size + "px",
+			"height": this.options.size + "px",
+			"border": "1px solid black"
+		});
 		
-		for (var r = 0; r < 8; r++) {
-			var row = $("<DIV></DIV").addClass("row");
-			this.element.append(row);
+		//Crear los 64 objetos que tendrán las celdas y sus referencias.
+		var celdas = this.celdas = [];
+		for (var i = 0; i < 8*8; i++) {
+			var light = ((i % 2) == (parseInt(i / 8) % 2));
+			var color = light ? "#FFFFFF" : "#000000";
 			
-			if (lastRow) {
-				row.position({
-					my: "top",
-					at: "bottom",
-					of: lastRow,
-					collision: "none"
-				});
-			} else {
-				row.position({
-					my: "left top",
-					at: "left top",
-					of: this.element,
-					collision: "none"
-				});
-			}			
-			
-			lastCol = null;
-			var piece;
-			for (var c = 0; c < 8; c++) {
-				var col = $("<DIV></DIV>").addClass("cell");
-				if ((r % 2) == (c % 2))  {
-					col.addClass("light");
-				} else {
-					col.addClass("dark");
-					col.droppable();
-					if (r<3) {
-						piece = $("<img src='img/blue.svg' width='100px' height='100px'></img>").draggable();
-						this.element.append(piece);
-					}
-					
-					if (r>5) {
-						piece = $("<img src='img/red.svg' width='100px' height='100px'></img>").draggable();
-						this.element.append(piece);
-					}
-				}
-				
-				row.append(col);				
-				
-				if (lastCol) {
-					col.position({
-						my: "left top",
-						at: "right top",
-						of: lastCol,
-					    collision: "none"
-					});
-				} else {
-					col.position({
-						my: "left top",
-						at: "left top",
-						of: row,
-					    collision: "none"
-					});
-					
-				}								
-							
-				$(piece).position({
-					my: "center center",
-					at: "center center",
-					of: col					
-				});	
-				
-				lastCol = col;
-			}	
-
-			lastRow = row;
+			var celda = celdas[i] = $("<DIV>").prop({
+				"id": "celda" + i
+			}).css({
+				"backgroundColor": color,
+				"width": cellsize + "px",
+				"height": cellsize + "px",
+				"position": "relative",
+				"float": "left"
+			});
+			if (!light) celda.droppable({
+				drop: thisWidget._drop
+			});
+			this._getMainDiv().append(celda);
 		}
+		
+		//Crear las 24 piezas
+		for (var i = 0; i < 24; i++) {
+			thisWidget._getObjsDiv().append(
+				$("<IMG>").prop({
+					"src": (i < 12) ? "img/blue.svg" : "img/red.svg"
+				}).css({
+					"width": parseInt(cellsize * .8) + "px",
+					"height": parseInt(cellsize * .8) + "px"
+				}).draggable({
+					revert: "invalid"
+				})
+			);
+		}
+	},
+	
+	_getMainDiv: function () {
+		return $("#tablaMain", this.element);
+	},
+	
+	_getObjsDiv: function () {
+		return $("#tablaObjs", this.element);
+	},
+	
+	_appendToElement: function(obj) {
+		this.element.append(obj);
+	},
+	
+	_drop: function(event, ui) {
+		ui.draggable.position({
+			my: "center",
+			at: "center",
+			of: $(event.target)
+		});
 	}
 	
-}
-);
+});
